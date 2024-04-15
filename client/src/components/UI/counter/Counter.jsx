4af65c00e106d3e25e90455116bd131b013a9bd2
Володимир Маселко <kartoshka8800@gmail.com>
2024-04-15 21:38:22 +0300

    new counter, now you can enter value from keyboard

--- client/src/components/UI/counter/Counter.jsx
+++ client/src/components/UI/counter/Counter.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import './Counter.scss';
 export default function Counter({ count, setCount, className }) {
-	const classNames = ["counter", className]
+	const maxValue = 9999999;
+	const classNames = ['counter', className];
+	function plus() {
+		if (count >= maxValue) return;
+		setCount(count + 1);
+	}
 	function minus() {
 		if (count <= 1) return;
 		setCount(count - 1);
 	}
+	function getValidValue(e) {
+		const res = parseInt(e.target.value);
+		if (isNaN(res) || res < 1) return 1;
+		if (res > maxValue) return maxValue;
+		return res;
+	}
 	return (
-		<div className={classNames.join(" ")}>
-			<button onClick={minus} className="counter__button counter__button_minus">
+		<div className={classNames.join(' ')}>
+			<button
+				onClick={minus}
+				className="counter__button counter__button_minus"
+			>
 				-
 			</button>
-			<div className='counter__content'>
-				<span className="counter__value">{count}</span>
-			</div>
+			<input
+				className="counter__value"
+				value={count}
+				onChange={(e) => setCount(getValidValue(e))}
+				onFocus={(e)=>e.target.select()}
+			/>
 			<button
-				onClick={() => setCount(count + 1)}
+				onClick={plus}
 				className="counter__button counter__button_plus"
 			>
 				+
